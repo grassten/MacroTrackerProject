a1840af47bd405c1c7cbefb12315dffd5cacf769
Steven <astenstevenc@gmail.com>
2018-03-11 10:27:23 -0400

    added user login, sqllite database, etc.

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template, request, url_for, flash, redirect
-from app import app
+from app import app, db
 import requests
 from jinja2 import Template
-from app.forms import SearchForm, LoginForm
+from app.forms import SearchForm, LoginForm, RegistrationForm
+from flask_login import login_required, logout_user, current_user, login_user
+from werkzeug.urls import url_parse
+from app.models import User
 
 
 @app.route('/')
+@login_required
 def home():
     return render_template('index.html')
 
 
 @app.route('/search', methods=['GET', 'POST'])
+@login_required
 def search():
     form = SearchForm()
     if request.method == 'GET':
@@ -74,8 +79,39 @@ def get_nutrition(ndbno):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect('/')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password.')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('home')
+        return redirect(next_page)
     return render_template('login.html', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    # logout_user is a flask_login function
+    logout_user()
+    return redirect(url_for('home'))
