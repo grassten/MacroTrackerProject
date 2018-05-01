68164bf45950e21f136d2cba890a3321319526eb
astensteven <Asten_Steven@bah.com>
2018-04-30 21:07:28 -0400

    styling

--- app/forms.py
+++ app/forms.py
@@ -73,8 +73,8 @@ class AddToDiaryForm(FlaskForm):
 class DiaryDatePicker(FlaskForm):
     date = StringField(default=datetime.utcnow().strftime(
         '%B %d, %Y'), validators=[DataRequired()])
-    back = SubmitField('Back')
-    forward = SubmitField('Forward')
+    back = SubmitField('<')
+    forward = SubmitField('>')
 
 
 class RemoveFood(FlaskForm):
