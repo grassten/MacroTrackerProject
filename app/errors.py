2be8220835258fdae801f939d0f806c83229a8b2
astensteven <Asten_Steven@bah.com>
2018-05-07 23:05:08 -0400

    404 and 500 error handling revision

--- app/errors.py
+++ app/errors.py
@@ -7,9 +7,7 @@ def not_found_error(error):
     return render_template('404.html'), 404
 
 
-def errorhandler(500):
-
-
+@app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('500.html'), 500
