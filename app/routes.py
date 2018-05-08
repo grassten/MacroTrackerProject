a94bdfd6c0763d3f19f21fa0c87afdd2b70ef58d
astensteven <Asten_Steven@bah.com>
2018-05-08 13:44:18 -0400

    multi add functionality from search

--- app/routes.py
+++ app/routes.py
@@ -36,7 +36,7 @@ def search(date=None, meal=None):
     if request.method == 'GET':
         current_userid = User.query.filter_by(id=current_user.get_id()).first()
         recent_foods = Food.query.distinct(Food.food_name).filter_by(
-            user_id=current_userid.id).order(Food.food_name)
+            user_id=current_userid.id).from_self().order_by(desc(Food.id))
 
         food_list_clean = []
         recent_list = True
