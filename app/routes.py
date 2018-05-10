c55e6b6664b33322c3848c4ccbab2c4aa6b4cb6a
astensteven <Asten_Steven@bah.com>
2018-05-09 22:21:35 -0400

    style

--- app/routes.py
+++ app/routes.py
@@ -305,7 +305,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('home')
+            next_page = url_for('diary')
         return redirect(next_page)
     return render_template('login.html', form=form)
 
